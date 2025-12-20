955288e438bdc53f4945ad72b923c6f404c7beb7
AvadhootSmart <1234avadhootsmart@gmail.com>
2025-12-21 00:48:57 +0530

    feat: added theme switching

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -2,13 +2,14 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
+import { ThemeProvider } from "next-themes";
+import { Navbar } from "@/components/navbar";
 
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
   weight: ["400", "500", "600", "700", "800", "900"],
 });
-
 
 export const metadata: Metadata = {
   title: "Journey",
@@ -22,12 +23,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${inter.variable} antialiased font-Inter`}
-      >
-        <main className="max-w-7xl mx-auto">
-          {children}
-        </main>
+      <head>
+        <script
+          async
+          crossOrigin="anonymous"
+          src="https://tweakcn.com/live-preview.min.js"
+        />
+      </head>
+      <body className={`${inter.variable} antialiased font-Inter bg-background`}>
+        <ThemeProvider attribute={"class"}>
+          <main className="max-w-7xl mx-auto">
+            <Navbar />
+            {children}
+          </main>
+        </ThemeProvider>
         <Toaster />
       </body>
     </html>
--- client/components/navbar.tsx
+++ client/components/navbar.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { ThemeToggle } from "./themeToggle";
+
 export const Navbar = () => {
-  return <nav className="">Journey</nav>;
+  return <nav className="flex justify-between items-center p-4 border-b border-muted-foreground"><h1>Journey</h1> <ThemeToggle /></nav>;
 };
